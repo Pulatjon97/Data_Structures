5585d7d37a695d70cd95c6b3910528269997864b
Pulatjon <polatsultonov97@gmail.com>
2024-12-03 04:38:44 +0900

    Maps

--- script.js
+++ script.js
@@ -51,6 +51,46 @@ const restaurant = {
   },
 };
 
+/*
+//////////////////
+//Maps
+const rest = new Map();
+rest.set("name", "Classico Italiano");
+rest.set(1, "Firenze, Italy");
+console.log(rest.set(2, "Lisbon, Portugal"));
+
+rest
+  .set("categories", ["Italian", "Pizzeria", "Vegetarian", "Organic"])
+  .set("open", 11)
+  .set("close", 23)
+  .set(true, "We are open :D")
+  .set(false, "We are close :(");
+
+console.log(rest.get("name"));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 21;
+console.log(rest.get(time > rest.get("open") && time < rest.get("close")));
+
+console.log(rest.has("categories"));
+rest.delete(2);
+// rest.clear();
+
+const arr = [1, 2];
+rest.set(arr, "Test");
+
+rest.set(document.querySelector("h1"), "Heading");
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr));
+//////////////////
+*/
+
+/*
+///////////////
+//Sets
 const ordersSet = new Set([
   "Pasta",
   "Pizza",
@@ -90,7 +130,7 @@ const nameArray = [...name1]; // Convert it into an array of characters
 const nameSize = nameArray.length; // Get the length of the array
 
 console.log(nameSize); // Output: 33
-
+*/
 /*
 ////////////////
 //Property NAMES
