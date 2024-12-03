dd38dda12b5c971239c2c0a05f5f94be2ae5ed50
Pulatjon <polatsultonov97@gmail.com>
2024-12-03 21:20:55 +0900

    Challenge #3

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
-/////////////////////////////////
-
+//////////////////////////////////////////////////////////////////////////
+/*
 const weekdays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
 const openingHours = {
   [weekdays[3]]: {
@@ -84,7 +84,8 @@ console.log(...question);
 console.log([...question.keys()]);
 console.log([...question.values()]);
 /*
-//////////////////
+
+///////////////////////////////////////////////////////////////////////////////////////////
 //Maps
 const rest = new Map();
 rest.set("name", "Classico Italiano");
@@ -678,3 +679,56 @@ team1 > team2 && console.log("Team2 is more likely to win");
 // Odd of victory Bayern Munich: 1.33
 // Odd of draw: 3.25
 // Odd of victory Borrussia Dortmund: 6.5
+
+//CHallenge#3
+// Let's continue with our football betting app! This time, we have a map called
+// 'gameEvents' (see below) with a log of the events that happened during the
+// game. The values are the events themselves, and the keys are the minutes in which
+// each event happened (a football game has 90 minutes plus some extra time).
+// Your tasks:
+// 1. Create an array 'events' of the different game events that happened (no
+// duplicates)
+// 2. After the game has finished, is was found that the yellow card from minute 64
+// was unfair. So remove this event from the game events log.
+// 3. Compute and log the following string to the console: "An event happened, on
+// average, every 9 minutes" (keep in mind that a game has 90 minutes)
+// 4. Loop over 'gameEvents' and log each element to the console, marking
+// whether it's in the first half o r second half (after 45 min) of the game, like this:
+// [FIRST HALF] 17:
+// ⚽
+// GOAL
+
+// const gameEvents = new Map([
+//   [17, "⚽ GOAL"],
+//   [36, "🔁 Substitution"],
+//   [47, "⚽ GOAL"],
+//   [61, "🔁 Substitution"],
+//   [64, "🔶 Yellow card"],
+//   [69, "🔴 Red card"],
+//   [70, "🔁 Substitution"],
+//   [72, "🔁 Substitution"],
+//   [76, "⚽ GOAL"],
+//   [80, "⚽ GOAL"],
+//   [92, "🔶 Yellow card"],
+// ]);
+// //1)
+// const events = [...new Set(gameEvents.values())];
+// console.log(events);
+// //2)
+// gameEvents.delete(64);
+// //3)
+// console.log(
+//   `An event happened, on average, every ${90 / gameEvents.size} minutes`
+// );
+// const time = [...gameEvents.keys()].pop();
+// console.log(time);
+// console.log(
+//   `An event happened, on average, every ${time / gameEvents.size} minutes`
+// );
+
+// //4)
+// for (const [min, event] of gameEvents) {
+//   const half = min <= 45 ? "FIRST" : "SECOND";
+//   console.log(`[HALF] ${min}: ${event}`);
+// }
+/////////////////////////////////////////////
